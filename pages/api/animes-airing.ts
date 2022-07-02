8667e270da25dbe38592ac7a24b165f81b86ce4a
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-07-02 16:38:12 -0300

    ✨ feat: add save anime to /api/animes and rule no-anonymous-default-export

--- pages/api/animes-airing.ts
+++ pages/api/animes-airing.ts
@@ -3,7 +3,6 @@ import clientPromise from '../../lib/mongodb';
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse<any>) => {
   try {
     const client = await clientPromise;
--- pages/api/animes.ts
+++ pages/api/animes.ts
@@ -3,16 +3,21 @@ import clientPromise from '../../lib/mongodb';
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse<any>) => {
   try {
     const client = await clientPromise;
 
     const db = client.db('Animes_Calendar');
 
-    const data = await db.collection('animes').find({}).toArray();
+    if (req.method == 'POST') {
+      const data = await db.collection('animes').insertOne(req.body);
 
-    res.status(200).json(data);
+      res.status(200).json(data);
+    } else {
+      const data = await db.collection('animes').find({}).toArray();
+
+      res.status(200).json(data);
+    }
   } catch (error) {
     res.status(500).json(error);
   }
